d989fc2b8fb198e45ccecd018e0b6d9fe78a0637
Dag Odenhall <dag.odenhall@gmail.com>
2011-07-27 17:50:10 +0200

    decouple string splitting and casing

--- braindump/metaprogramming.py
+++ braindump/metaprogramming.py
@@ -2,19 +2,88 @@ import keyword
 import re
 
 
+class Split(object):
+
+    def __init__(self, string):
+        self._string = unicode(string)
+
+    @property
+    def whitespace(self):
+        return self._string.split()
+
+    @property
+    def words(self):
+        return re.findall(ur'[A-Za-z0-9]+', self._string)
+
+    @property
+    def mixed(self):
+        return re.findall(ur'.+?(?=[A-Z]|$)', self._string)
+
+    @property
+    def camel(self):
+        return re.findall(ur'[A-Z][^A-Z]*', self._string)
+
+    @property
+    def initials(self):
+        return re.findall(ur'[A-Z].*?(?=[A-Z][^A-Z]|$)', self._string)
+
+    @property
+    def smart(self):
+        pattern = ur"""
+            [a-z0-9]+
+          | [A-Z] [a-z0-9]+
+          | [A-Z0-9]+? (?= [A-Z] [a-z0-9] )
+          | [A-Z0-9]+
+        """
+        return re.findall(pattern, self._string, re.X)
+
+
+class Casing(object):
+
+    def __init__(self, words):
+        self._words = map(unicode, words)
+
+    def __repr__(self):
+        return u'Casing({0!r})'.format(self._words)
+
+    @property
+    def upper(self):
+        return u'_'.join(self._words).upper()
+
+    @property
+    def lower(self):
+        return u'_'.join(self._words).lower()
+
+    @property
+    def mixed(self):
+        camel = self.camel
+        return camel[0].lower() + camel[1:]
+
+    @property
+    def camel(self):
+        return u''.join(map(unicode.capitalize, self._words))
+
+    @property
+    def acronymic(self):
+        return u''.join(word[0].upper() + word[1:] for word in self._words)
+
+    @property
+    def ident(self):
+        ident = Casing(Split(u','.join(self._words)).words).lower
+        if ident[0].isdigit():
+            return u'_' + ident
+        if keyword.iskeyword(ident):
+            return ident + u'_'
+        return ident
+
+    @property
+    def slug(self):
+        return self.lower.replace(u'_', u'-')
+
+
 def string_to_identifier(string):
-    ident = re.sub(r'[^a-zA-Z0-9]+', '_', string)
-    ident = ident.strip('_')
-    if ident[0].isdigit():
-        ident = '_' + ident
-    if keyword.iskeyword(ident):
-        ident += '_'
-    return ident
+    return Casing(Split(string).words).ident
 
 
 def identifier_to_string(identifier):
-    string = re.sub(r'([A-Z][^A-Z_])', r'_\1', identifier)
-    string = string.strip('_')
-    string = re.sub(r'_+', '-', string)
-    string = string.lower()
-    return unicode(string)
+    return Casing(Split(identifier).smart).slug
--- tests/test_metaprogramming.py
+++ tests/test_metaprogramming.py
@@ -11,3 +11,22 @@ def test_identifier_to_string(identifier_to_string):
     for identifier, string in identifier_to_string.iteritems():
         result = metaprogramming.identifier_to_string(identifier)
         assert result == unicode(string)
+
+
+def test_splitting(splits):
+    for string, variants in splits.iteritems():
+        splitter = metaprogramming.Split(string)
+        for variant, value in variants.iteritems():
+            split = getattr(splitter, variant)
+            assert split == value
+            for word in split:
+                assert type(word) is unicode
+
+
+def test_casing(casings):
+    casing = metaprogramming.Casing(casings['words'])
+    if 'repr' in casings:
+        assert repr(casing) == casings['repr']
+    for case, value in casings['cases'].iteritems():
+        cased = getattr(casing, case)
+        assert cased == value and type(cased) is unicode
