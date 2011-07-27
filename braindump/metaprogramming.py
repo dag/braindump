481a408c4d0b67a0650884bed8434eaa21c78594
Dag Odenhall <dag.odenhall@gmail.com>
2011-07-27 02:35:42 +0200

    convert python identifiers to string slugs

--- braindump/metaprogramming.py
+++ braindump/metaprogramming.py
@@ -2,7 +2,7 @@ import keyword
 import re
 
 
-def to_identifier(string):
+def string_to_identifier(string):
     ident = re.sub(r'[^a-zA-Z0-9]+', '_', string)
     ident = ident.strip('_')
     if ident[0].isdigit():
@@ -10,3 +10,11 @@ def to_identifier(string):
     if keyword.iskeyword(ident):
         ident += '_'
     return ident
+
+
+def identifier_to_string(identifier):
+    string = re.sub(r'([A-Z][^A-Z_])', r'_\1', identifier)
+    string = string.strip('_')
+    string = re.sub(r'_+', '-', string)
+    string = string.lower()
+    return unicode(string)
--- tests/test_metaprogramming.py
+++ tests/test_metaprogramming.py
@@ -1,6 +1,13 @@
 from braindump import metaprogramming
 
 
-def test_encoding_identifiers(string_to_identifier):
+def test_string_to_identifier(string_to_identifier):
     for string, identifier in string_to_identifier.iteritems():
-        assert metaprogramming.to_identifier(unicode(string)) == identifier
+        result = metaprogramming.string_to_identifier(unicode(string))
+        assert result == identifier
+
+
+def test_identifier_to_string(identifier_to_string):
+    for identifier, string in identifier_to_string.iteritems():
+        result = metaprogramming.identifier_to_string(identifier)
+        assert result == unicode(string)
